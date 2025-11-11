cc50edb5b793cf615d3aed5efaabfbf98dadda8f
NishantMummithi <nishant.mummithi@gmail.com>
2025-11-11 21:37:31 +0530

    Implement rainfall classification based on input
    Fix conditional statements for rainfall classification.

---
+++ Rainfall.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int x;
+  scanf("%d",&x);
+  if(x<3)
+{
+   printf("LIGHT");
+}
+else if
+{
+   printf("MODERATE");
+}
+else
+{
+   printf("HEAVY");
+}
+return 0;
+}
+   
