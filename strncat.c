63da1e970f5935de1b134db893acc7212ec133a1
NishantMummithi <nishant.mummithi@gmail.com>
2025-11-12 19:41:54 +0530

    strncat.c example for string concatenation

---
+++ strncat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[]="Hello";
+    char s2[]="World";
+    printf("%s",s1);
+    strncat(s1,s2,3);
+    printf("%s",s1);
+    return 0;
+}
