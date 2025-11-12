96ff59e0fcc2bf1622fe04e8e179f4e49c667840
NishantMummithi <nishant.mummithi@gmail.com>
2025-11-12 20:13:35 +0530

    strcmp example program in strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[]="ABCD";
+    char s2[]="ABCE";
+    int res;
+    res=strcmp(s1,s2);
+    if(res==0)
+    {
+        printf("teo strings are equal");
+    }
+    else if(res<0)
+    {
+        printf("%s is smaller than %s",s1,s2);
+    }
+    else
+    {
+        printf("%s is larger than %s",s1,s2);
+    }
+    return 0;
+}
