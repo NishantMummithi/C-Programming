793102ae2d684c5ee8065d7ef7383d765c3bd0b1
NishantMummithi <nishant.mummithi@gmail.com>
2025-11-13 21:52:11 +0530

    Grades based on percentage

---
+++ Grades.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+  int p,ch,b,m,c;
+  scanf("%d%d%d%d%d",&p,&ch,&b,&m,&c);
+  float per;
+  per=((p+ch+b+m+c)*100)/500;
+  if(per>=90)
+{
+   printf("Grade A");
+}
+else if(per>=80)
+{
+   printf("Grade B");
+}
+else if(per>=70)
+{
+   printf("Grade C");
+}
+else if(per>=60)
+{
+   printf("Grade D");
+}
+else if(per>=40)
+{
+   printf("Grade E");
+}
+else if(per<40)
+{
+   printf("Grade F");
+}
+return 0;
+}
